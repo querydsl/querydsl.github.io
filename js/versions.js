7293090287e6d4526107e911634e2cd17cc69aef
John Tims <john.k.tims@gmail.com>
2015-04-11 09:08:27 -0400

    Use html_single by default

--- js/versions.js
+++ js/versions.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     "3.6.3","3.6.2","3.6.1","3.6.0",
     "3.5.1","3.5.0",
     "3.4.3","3.4.2","3.4.1","3.4.0",
-    "3.3.4","3.3.3","3.3.2","3.3.1","3.3.0", 
+    "3.3.4","3.3.3","3.3.2","3.3.1","3.3.0",
     "3.2.4","3.2.3","3.2.2","3.2.1","3.2.0",
     "3.1.1","2.1.0","3.0.0",
     "2.9.0","2.8.0","2.7.0","2.6.0","2.5.0","2.4.0","2.3.0","2.2.0","2.1.0","2.0.0"];
@@ -13,21 +13,19 @@ $(document).ready(function() {
     $("#docs .version").append(opt);
   });
 
-  $("#docs .version").change(function() {
-    var version = $(this).val();
+  var updateDocumentationVersion = function(version) {
     var prefix = "http://www.querydsl.com/static/querydsl/" + version;
-    $("#docs .docs").attr("href", prefix + "/reference/html/");
-    $("#docs .korean").attr("href", prefix + "/reference/ko-KR/html");
-    // var pdf = prefix + "/reference/pdf/"; 
+    $("#docs .docs").attr("href", prefix + "/reference/html_single/");
+    $("#docs .korean").attr("href", prefix + "/reference/ko-KR/html_single");
+    // var pdf = prefix + "/reference/pdf/";
     $("#docs .javadocs").attr("href", prefix + "/apidocs/");
-    $("#docs .downloads").attr("href", prefix)
+    $("#docs .downloads").attr("href", prefix);
+  };
+
+  $("#docs .version").change(function() {
+    var selectedVersion = $(this).val();
+    updateDocumentationVersion(selectedVersion);
   });
 
-  var prefix = "http://www.querydsl.com/static/querydsl/" + versions[0];
-  $("#docs .docs").attr("href", prefix + "/reference/html/");
-  $("#docs .korean").attr("href", prefix + "/reference/ko-KR/html");
-  // var pdf = prefix + "/reference/pdf/"; 
-  $("#docs .javadocs").attr("href", prefix + "/apidocs/");
-  $("#docs .downloads").attr("href", prefix)
-
+  updateDocumentationVersion(versions[0]);
 });
